8c8abb6fd1d375fca59ab77f6a6d2b3c5643cc60
marwasabon <marwasabon@gmail.com>
2023-09-07 21:07:06 +0000

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-
-    print("{:d} argument".format(len(sys.argv) - 1), end="")
-    if len(sys.argv) == 1:
+    from sys import argv
+    print("{:d} argument".format(len(argv) - 1), end="")
+    if len(argv) == 1:
         print(".")
-    elif len(sys.argv) == 2:
+    elif len(argv) == 2:
         print(":")
     else:
         print("s:")
 
-    for i in range(len(sys.argv) - 1):
-        print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
+    for i in range(len(argv) - 1):
+        print("{:d}: {:s}".format(i + 1, argv[i + 1]))
