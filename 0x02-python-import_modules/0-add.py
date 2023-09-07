3dbd40b66c228d66be225c7effa98ca04157c4fd
marwasabon <marwasabon@gmail.com>
2023-09-07 20:45:31 +0000

    task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,9 +2,6 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    def add(a, b):
         a = 1
         b = 2
-        result = (a + b)
-        print(f"{a} + {b} = {result}")
-    add(a, b)
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
