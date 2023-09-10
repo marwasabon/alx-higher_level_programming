dedf2b342d315b05df0c7763f3924d59872a4f8f
marwasabon <marwasabon@gmail.com>
2023-09-10 14:50:49 +0000

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list.copy()
+    if inx >= len(my_list) or inx < 0:
+        return new_list
+
+    new_list[inx] = element
+    return new_list
