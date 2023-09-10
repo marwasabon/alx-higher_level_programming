0de9791c3b6aa92c95568db2479f2e7d6877ee13
marwasabon <marwasabon@gmail.com>
2023-09-10 14:56:59 +0000

    task 5

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# use char
+def no_c(my_string):
+    new_string = ""
+    for char in my_string:
+        if char != 'c' and char != 'C':
+            new_string += char
+
+            return new_string
