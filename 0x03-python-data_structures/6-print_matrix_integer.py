abe31a245df748d5fa6e699119e55d83c466f31d
marwasabon <marwasabon@gmail.com>
2023-09-10 15:17:20 +0000

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print(" ".join(str(num) for num in row))
+    str = " "
+    for row in range(len(matrix)):
+        for co in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][co]), end="")
+            if co != len(matrix[row]) - 1:
+                print(" ", end="")
+        print()
