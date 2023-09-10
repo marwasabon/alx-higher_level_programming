177c30e525e61fefa54e0ebb9a246bb70b3c3d46
marwasabon <marwasabon@gmail.com>
2023-09-10 17:19:06 +0000

    task 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # ttest if null
 def max_integer(my_list=[]):
-    if my_list is None or len(my_list) is 0:
+    if my_list is [] or len(my_list) is 0:
         return None
 
     max = my_list[0]
