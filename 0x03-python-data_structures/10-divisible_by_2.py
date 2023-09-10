2705ebcdd56ca4854ac662260fc634572d5a6778
marwasabon <marwasabon@gmail.com>
2023-09-10 17:44:08 +0000

    task 10s

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-
+    new_list = []
     if len(my_list) is 0:
         return new_list
 
@@ -9,4 +9,5 @@ def divisible_by_2(my_list=[]):
             new_list.append(True)
         else:
             new_list.append(False)
+
     return new_list
