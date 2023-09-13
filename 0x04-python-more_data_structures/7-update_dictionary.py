ec4727141eab050d5b5757a47b7476c8538f1646
marwasabon <marwasabon@gmail.com>
2023-09-13 19:50:03 +0000

    task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+def update_dictionary(my_dict, key, value):
+
+    if key in my_dict:
+        del my_dict[key]
+        my_dict.update({key: value})
+
+    else:
+        my_dict.update({key: value})
+        return my_dict
