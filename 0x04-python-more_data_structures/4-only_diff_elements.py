468f6ed49bd4a41e7fc1cc1b8a5f9b975b6c4263
marwasabon <marwasabon@gmail.com>
2023-09-13 19:30:31 +0000

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,8 +3,8 @@ def only_diff_elements(set_1, set_2):
     temp = list(set_1) + list(set_2)
     new_set = set()
 
-    for i in range(len(temp)):
-        if temp_list.count(temp[i]) == 1:
-            new_set.add(temp[i])
+    for i in temp:
+        if temp_list.count(i) == 1:
+            new_set.add(i)
 
     return set(new_set)
