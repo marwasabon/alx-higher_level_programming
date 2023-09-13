9a08c3c40691ab9ef39b0dff30ddc80f2b3b4db7
marwasabon <marwasabon@gmail.com>
2023-09-13 13:58:14 +0000

    task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-def square(x):
-    return x * x
-
-
 def square_matrix_simple(matrix=[]):
+    rows = len(matrix)
+    cols = len(matrix[0])
 
-    new_list = matrix[:]
+    result = []
 
-    for i in range(len(new_list)):
-        new_list[i] = map(square, new_list[i])
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            row.append(matrix[i][j] ** 2)
+            result.append(row)
 
-    return new_list
+        return result
