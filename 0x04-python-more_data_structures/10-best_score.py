d5837cd839d504b3ff6208540e6bfec51e8001f2
marwasabon <marwasabon@gmail.com>
2023-09-15 18:47:35 +0000

    task 10-

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,10 +4,11 @@ def best_score(a_dictionary):
     if (not a_dictionary or len(list(a_dictionary.keys())) == 0):
 
         return None
+
     start = 0
 
     for key in a_dictionary:
-        if start is 0:
+        if start == 0:
             holder = key
             score = a_dictionary[key]
             start = 1
