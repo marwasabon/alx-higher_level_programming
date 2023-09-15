1568de116d02a3a428f16578fb76bfd321818b01
marwasabon <marwasabon@gmail.com>
2023-09-15 18:28:41 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,13 +4,16 @@ def best_score(a_dictionary):
     if a_dictionary is None or len(list(my_dict.keys())) is 0:
 
         return None
+    start = 0
 
-    max_score = float('-inf')
-    best_key = None
+    for key in my_dict:
+        if start is 0:
+            holder = key
+            score = my_dict[key]
+            start = 1
 
-    for key, value in a_dictionary.items():
-        if value > max_score:
-            max_score = value
-            best_key = key
+        if my_dict[key] > score:
+            score = my_dict[key]
+            holder = key
 
-    return best_key
+    return holdergg
