8a237888daf506e9020e26e08c2efc2a2cb03b57
marwasabon <marwasabon@gmail.com>
2023-09-15 19:45:19 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-
     if not (isinstance(roman_string, str)):
         return 0
-    if not roman_string:
-        return 0
+
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
 
@@ -13,4 +11,5 @@ def roman_to_int(roman_string):
             result += roman_dict[roman_string[i]] - 2 * roman_dict[roman_string[i-1]]
         else:
             result += roman_dict[roman_string[i]]
+
     return result
