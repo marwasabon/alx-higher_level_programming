50f359ccbad27f8fbb76e088af2613649f6689ba
marwasabon <marwasabon@gmail.com>
2023-09-15 20:24:21 +0000

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not (isinstance(roman_string, str)):
+                return 0
     roman_dict = dict(M=1000, C=100, D=500, X=10, L=50, V=5, I=1)
     result = 0
     prev = 0
