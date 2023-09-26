371dfbae8c879066a45263e4d11e860cfd4ce675
marwasabon <marwasabon@gmail.com>
2023-09-26 21:37:20 +0000

    task 0

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 class Square:
     """
+    This is a Python script that defines a class named Square.
+    """
+    """
     This is square class. Idoesnt do anything
 
-    Attributes:
-        None
+    Attributes:None
 
-     Methods:
-        None
+     Methods:None
 
      """
     '''what is qrong here pls'''
