1cc905720103cad9aae8fdc5ac77c93d7051ca79
marwasabon <marwasabon@gmail.com>
2023-09-26 21:51:26 +0000

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''This is a documention for this class
+This is a Python script that defines a class named Square.
+'''
+
+
+class Square:
+    '''
+     This is a Python script that defines a class named Square.
+     This is square class. Idoesnt do anything
+     Attributes:None
+    Methods:None
+    '''
+    def __init__(self, size):
+        self.__size = size
