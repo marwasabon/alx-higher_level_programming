987c08b370e19c36054d656c6fee5e1477070330
marwasabon <marwasabon@gmail.com>
2023-10-03 10:40:39 +0000

    Task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -110,9 +110,7 @@ class Rectangle:
 
         if self.__width == 0 or self.__height == 0:
             return ""
-        else:
-            rectangle_str = ""
-
+        rectangle_str = ""
         for _ in range(self.__height):
             rectangle_str += "#" * self.__width + "\n"
 
