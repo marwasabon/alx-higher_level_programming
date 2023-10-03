4c9fd84bc61bef8a69b581fa1f7f2ff83d3c2c55
marwasabon <marwasabon@gmail.com>
2023-10-03 16:39:39 +0000

    this tasks

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,11 +4,25 @@ This is a Python script that defines add_integer(a, b)
 """
 
 
-def add_integer(a, b=0):
+def add_integer(a, b=98):
     """
     add_integer:
     Returns the sum of a and b, cast as int if float
     raise error if not int or float types
+    >>> add_integer(2, 3)
+    5
+
+    >>> add_integer(-5, 10)
+    5
+
+    >>> add_integer(0, 0)
+    0
+
+    >>> add_integer(100, -50)
+    50
+
+    >>> add_integer(0, 5)
+    5
     """
 
     if isinstance(a, (int, float)) is False:
