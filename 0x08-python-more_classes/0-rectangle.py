ca7533e04b279756698cb59768c635f3991d1da1
marwasabon <marwasabon@gmail.com>
2023-10-03 08:40:34 +0000

    Task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,21 @@
 #!/usr/bin/python3
+"""
+This is a Python script that defines a class named Square.
+
+"""
+
+
 class Rectangle:
+    """
+    This is a Python script that defines a class named Rectangle.
+    """
+    """
+    This is Rectangle class. Idoesnt do anything
+
+    Attributes:None
+
+    Methods:None
+
+    """
+    '''what is qrong here pls'''
     pass
