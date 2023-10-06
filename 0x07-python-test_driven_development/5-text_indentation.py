e52a2937d94b2be26764afdfc16085c340b98202
marwasabon <marwasabon@gmail.com>
2023-10-06 22:18:08 +0000

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -4,6 +4,12 @@ This is a Python script that defines add_integer(a, b)
 """
 
 def text_indentation(text):
+    """
+    text_indentation:
+    This function prints a text with 2 new 
+    lines after each of these characters: ., ? and :
+    """
+
     if not isinstance(text, str):
         raise TypeError("text must be a string")
     for char in ".:?":
