658db614e43e32bbd5485f5bc44c26d55da869cc
marwasabon <marwasabon@gmail.com>
2023-10-10 10:27:56 +0000

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is a Python script that defines a class named mylist..
+
+"""
+
+
+def is_same_class(obj, a_class):
+    '''
+    Write a class MyList that inherits from list:
+    '''
+    return type(obj) is a_class
