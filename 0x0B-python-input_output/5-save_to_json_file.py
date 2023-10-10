397ed2c7c81fbaa0c0d2c52ca5f6350bec58349f
marwasabon <marwasabon@gmail.com>
2023-10-10 17:41:33 +0000

    task 5

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,6 +14,6 @@ def save_to_json_file(my_obj, filename):
     a text file, using a JSON representation:
     '''
 
-    with open(filename, ‘w’) as file:
+    with open(filename, 'w') as file:
 
         json.dump(my_obj, file)
