bc6322eb6d39eb1259a6832f22cabfefd19aed7e
marwasabon <marwasabon@gmail.com>
2023-10-10 19:58:30 +0000

    task 10

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -21,7 +21,8 @@ class Student:
 
         self.age = age
 
-    def to_json(self):
+    def to_json(self, attrs=None):
+
         '''
         function that returns the dictionary
         description with simple data structure (list, dictionary, string,
@@ -30,4 +31,8 @@ class Student:
         if attrs is None:
             return self.__dict__
         else:
-            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
+            return {
+                    attr: getattr(self, attr)
+                    for attr in attrs
+                    if hasattr(self, attr)
+                    }
