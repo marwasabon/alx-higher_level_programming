18a48a596f689a2cdc05437728b7bbf6f3f98bd8
marwasabon <marwasabon@gmail.com>
2023-10-10 19:18:34 +0000

    task 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -11,22 +11,4 @@ def class_to_json(obj):
     description with simple data structure (list, dictionary, string,
     integer and boolean) for JSON serialization of an object:
     '''
-    if isinstance(obj, list):
-        return obj
-    elif isinstance(obj, dict):
-        return obj
-    elif isinstance(obj, str):
-        return obj
-    elif isinstance(obj, int):
-        return obj
-    elif isinstance(obj, bool):
-        return obj
-    elif hasattr(obj, '__dict__'):
-        return {
-                key: class_to_json(value)
-                for key, value in obj.__dict__.items()
-                }
-    else:
-        return None
-    else:
-        return None
+    return obj.__dict__
