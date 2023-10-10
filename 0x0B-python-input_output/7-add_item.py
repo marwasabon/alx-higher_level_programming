6936cf53b806550dfbb309badddf2cd2eff34c6c
marwasabon <marwasabon@gmail.com>
2023-10-10 19:02:48 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,24 +6,24 @@ This is a Python script..
 
 import json
 import sys
+from os import path
+
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
 filename = "add_item.json"
-'''
-this os new
-'''
+if path.exists(filename):
+    try:
+        my_list = load_from_json_file(filename)
 
-try:
-    my_list = load_from_json_file(filename)
-
-except FileNotFoundError:
-    my_list = []
+    except FileNotFoundError:
+        my_list = []
 
 # Add all arguments to the list
-    my_list.extend(sys.argv[1:])
+else:
+    my_list = []
+my_list.extend(sys.argv[1:])
 
 # Save the updated list to the file
-    save_to_json_file(my_list, filename)
+save_to_json_file(my_list, filename)
