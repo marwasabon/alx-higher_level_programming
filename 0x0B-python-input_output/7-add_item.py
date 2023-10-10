a4ee4ee05b07b23fa6736281875c3c5099f04def
marwasabon <marwasabon@gmail.com>
2023-10-10 18:49:44 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,8 +7,9 @@ This is a Python script..
 import json
 import sys
 
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
 
 filename = "add_item.json"
 '''
