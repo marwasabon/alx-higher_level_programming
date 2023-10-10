b9909a0f0a19d8b9754105ef2b93fd84804d3fba
marwasabon <marwasabon@gmail.com>
2023-10-10 12:07:25 +0000

    task 8

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -5,7 +5,7 @@ This is a Python script that defines a class named mylist..
 """
 
 
-class BaseGeometry(BaseGeometry):
+class Rectangle(BaseGeometry):
 
     """
     A base class for geometrical shapes.
