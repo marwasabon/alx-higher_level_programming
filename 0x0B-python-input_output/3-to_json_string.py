f677549a89e1a3f528b8bc8baec49f9cf07789b0
marwasabon <marwasabon@gmail.com>
2023-10-10 17:33:45 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This is a Python script..
+
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    Write a function that returns the JSON
+    representation of an object (string):
+    '''
+    return json.dumps(my_obj)
