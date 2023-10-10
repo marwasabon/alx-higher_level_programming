1cd3f1a1ac638c57bc4019301eca5c1b2c7c7077
marwasabon <marwasabon@gmail.com>
2023-10-10 17:52:32 +0000

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,6 +5,7 @@ This is a Python script..
 """
 
 import json
+import sys
 
 
 def load_from_json_file(filename):
@@ -13,10 +14,16 @@ def load_from_json_file(filename):
     Python list, and then save them to a file:
     '''
 
-    with open(filename, 'r') as file:
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            return json.load(f)
+        except FileNotFoundError:
+            return []
 
-        json_string = file.read()
-
-        obj = json.loads(json_string)
-
-        return obj
+        def save_to_json_file(my_obj, filename):
+            with open(filename, 'w', encoding='utf-8') as f:
+                json.dump(my_obj, f)
+                filename = "add_item.json"
+                my_list = load_from_json_file(filename)
+                my_list.extend(sys.argv[1:])
+                save_to_json_file(my_list, filename)
