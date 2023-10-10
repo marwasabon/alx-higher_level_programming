78389b9fb8b3f2b6bd7aef8614a6d32422f96a10
marwasabon <marwasabon@gmail.com>
2023-10-10 17:23:10 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,10 @@ This is a Python script..
 
 
 def write_file(filename="", text=""):
+    '''
+    unction that writes a string to a text file (UTF8)
+    returns the number of characters written:
+    '''
 
     try:
         with open(filename, "w", encoding="utf-8") as file:
