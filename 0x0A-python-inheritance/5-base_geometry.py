402e24c42b11aa873acdd98e5f097addd30c3a80
marwasabon <marwasabon@gmail.com>
2023-10-10 10:36:51 +0000

    task 5

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/piython3
+"""
+This is a Python script that defines a class named mylist..
+
+"""
+
+
+class BaseGeometry:
+    """
+    This is a Python script that defines a class named mylist..
+
+    """
+    pass
