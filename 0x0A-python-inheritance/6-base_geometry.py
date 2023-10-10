dd809b5f30382f20b6a929c036f8a12dbc4a914e
marwasabon <marwasabon@gmail.com>
2023-10-10 10:42:05 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is a Python script that defines a class named mylist..
+
+"""
+
+
+class BaseGeometry:
+    """
+    A base class for geometrical shapes.
+    """
+
+    def area(self):
+
+        """
+        Calculates the area of the shape.
+        This method is not implemented in the base class.
+        Raises:
+        Exception: "area() is not implemented"
+        """
+
+        raise Exception("area() is not implemented")
