781159373ecf0964a99c0e89645022253f6b0d9f
marwasabon <marwasabon@gmail.com>
2023-10-10 03:22:22 +0000

    task 0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python3
+"""
+This is a Python script that defines a class named Square.
+
+"""
+
+
 def lookup(obj):
+    '''
+    function that returns the list of available
+    attributes and methods of an object
+    '''
     return dir(obj)
