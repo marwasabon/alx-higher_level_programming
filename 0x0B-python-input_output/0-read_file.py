9f3b2f4c0adfa3f720d03447e8aafd8e88760fee
marwasabon <marwasabon@gmail.com>
2023-10-10 17:11:00 +0000

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a Python script..
+
+"""
+
+
+def read_file(filename=""):
+    '''
+    Write a function that reads a text file (UTF8) and prints it to stdout:
+    '''
+    with open(filename, 'r', encoding='utf-8') as f:
+        print(f.read())
