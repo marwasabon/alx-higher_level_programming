2e51e4babbef2d2cafdd7605f024fffdee1ce3a6
marwasabon <marwasabon@gmail.com>
2023-10-10 10:11:04 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is a Python script that defines a class named mylist..
+
+"""
+
+
+class MyList(list):
+    '''
+    Write a class MyList that inherits from list:
+    '''
+    def print_sorted(self):
+        print(sorted(self))
