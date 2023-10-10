5ae9e30e9efe3c0f51f26786adbe6aa36ceb4e85
marwasabon <marwasabon@gmail.com>
2023-10-10 10:31:13 +0000

    task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -7,8 +7,7 @@ This is a Python script that defines a class named mylist..
 
 def is_kind_of_class(obj, a_class):
     """
-    This is a Python script that defines a function named 
+    This is a Python script that defines a function named
     is_kind_of_class
-
     """
     return isinstance(obj, a_class)
