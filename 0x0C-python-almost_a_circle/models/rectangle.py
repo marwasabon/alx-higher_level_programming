06383fd87ec55ffc976f07e71d493aee351555a3
marwasabon <marwasabon@gmail.com>
2023-10-16 12:53:30 +0300

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -34,6 +34,21 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
 
+    def update(self, *args):
+        '''
+        assigns a key/value argument to attributes
+        '''
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4
+
     def update(self, *args, **kwargs):
         '''
         assigns a key/value argument to attributes
