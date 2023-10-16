1c13930d6ae3c046f276cf27ae7bb0fbea448146
marwasabon <marwasabon@gmail.com>
2023-10-16 20:47:31 +0300

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -17,6 +17,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        '''
+         returns the JSON string representation of list_dictionaries:
+        '''
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries
+        return json.dumps(list_dictionaries)
