48764df724d4424be787219043c9ba10674e076d
marwasabon <marwasabon@gmail.com>
2023-10-16 13:41:59 +0300

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -31,6 +31,29 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """
+        Getter method for size.
+        Returns:
+            int: The size of the square.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Setter method for size.
+        Args:
+            value (int): The size of the square.
+        Raises:
+            ValueError: If value is not a positive integer.
+        """
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError("width must be an integer greater than 0")
+        self.width = value
+        self.height = value
+
     def __str__(self):
         '''
         returns [Square] (<id>) <x>/<y> - <width>/<height>
