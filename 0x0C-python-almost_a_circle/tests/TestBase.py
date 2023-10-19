7ff53dea77a07cf6d2b055e9962a230fe4adc286
marwasabon <marwasabon@gmail.com>
2023-10-19 12:39:02 +0300

    Update TestBase.py

--- 0x0C-python-almost_a_circle/tests/TestBase.py
+++ 0x0C-python-almost_a_circle/tests/TestBase.py
@@ -2,21 +2,23 @@
 import unittest
 import pep8
 from models.base import Base
+
+
 class TestBase(unittest.TestCase):
         """
         Test that all files.
         """
         def test_pep8_conformance(self):
-        """
-        Test that all files.
-        """
+                """
+                Test that all files.
+                """
         style = pep8.StyleGuide()
         result = style.check_files(['models/base.py'])
         self.assertEqual(result.total_errors, 0)
         def test_to_json_string(self):
-            """
-            Test the to_json_string method.
-            """
+                """
+                Test the to_json_string method.
+                """
             # Test with an empty list
             list_dictionaries = []
             json_string = Base.to_json_string(list_dictionaries)
@@ -29,9 +31,9 @@ class TestBase(unittest.TestCase):
             self.assertEqual(json_string, expected_json_string)
         
         def test_save_to_file(self):
-            """
-            Test the save_to_file method.
-            """
+                """
+                Test the save_to_file method.
+                """
             # Test with an empty list
             list_objs = []
             Base.save_to_file(list_objs)
@@ -48,9 +50,9 @@ class TestBase(unittest.TestCase):
                 self.assertEqual(json_string, expected_json_string)
         
         def test_from_json_string(self):
-            """
-            Test the from_json_string method.
-            """
+                """
+                Test the from_json_string method.
+                """
             # Test with an empty string
             json_string = ""
             list_dictionaries = Base.from_json_string(json_string)
@@ -63,9 +65,9 @@ class TestBase(unittest.TestCase):
             self.assertEqual(list_dictionaries, expected_list_dictionaries)
                     
         def test_create(self):
-            """
-            Test the create method.
-            """
+                """
+                Test the create method.
+                """
             # Test with a Rectangle
             rectangle_dict = {'id': 1, 'width': 2, 'height': 3}
             rectangle = Base.create(**rectangle_dict)
@@ -80,9 +82,9 @@ class TestBase(unittest.TestCase):
             self.assertEqual(square.size, 4)
         
         def test_load_from_file(self):
-            """
-            Test the load_from_file method.
-            """
+                """
+                Test the load_from_file method.
+                """
             # Test with an existing file
             list_objs = [Base(1), Base(2)]
             Base.save_to_file(list_objs)
