808d9c8b71571751cc3a845ec933321443a3c1a9
marwasabon <marwasabon@gmail.com>
2023-11-14 15:48:02 +0000

    Task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# this is my sql 
+-- Write a script that prints thei full description of the 
+-- table first_table from thei database hbtn_0c_0 in your MySQL server.
+INSERT INTO first_table (id, name) VALUES (89, 'Best School');
