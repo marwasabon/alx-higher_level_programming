dadc6c7e1eca38e112d4eb998b18424f9d7d1c8e
marwasabon <marwasabon@gmail.com>
2023-11-14 15:55:31 +0000

    Task 10

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# this is my sql 
+-- Write a script that prints thei full description of the 
+-- table first_table from thei database hbtn_0c_0 in your MySQL server.
+SELECT score, name FROM second_table ORDER BY score DESC;
+
