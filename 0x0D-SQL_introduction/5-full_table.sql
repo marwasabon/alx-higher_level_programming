9a80c694ce0b0257852fe01182681484cb9bd9fb
marwasabon <marwasabon@gmail.com>
2023-11-14 15:32:20 +0000

    Task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# this is my sql 
+-- This script that Write a script that creates a table called first_table 
+-- in the current database in your MySQL server.
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256));
