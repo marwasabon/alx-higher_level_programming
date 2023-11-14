28a41e0d394633c627e333f79336887778fd6929
marwasabon <marwasabon@gmail.com>
2023-11-14 15:39:19 +0000

    Task 5

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # this is my sql 
--- Write a script that prints the full description of the 
--- table first_table from the database hbtn_0c_0 in your MySQL server.
+-- Write a script that prints thei full description of the 
+-- table first_table from thei database hbtn_0c_0 in your MySQL server.
 SHOW FULL COLUMNS FROM first_table;
 
