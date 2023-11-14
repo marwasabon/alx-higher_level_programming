1ad0d1aa1888495b53fb665381f6e3259f2fb46e
marwasabon <marwasabon@gmail.com>
2023-11-14 16:00:54 +0000

    Task 12

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# this is my sql 
+-- Write a script that prints thei full description of the 
+-- table first_table from thei database hbtn_0c_0 in your MySQL server.
+UPDATE second_table SET score = 10 WHERE name = 'Bob';
