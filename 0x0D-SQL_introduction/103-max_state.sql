3ab844156b6474907dc76bcfd30b1c9e45f47bc1
marwasabon <marwasabon@gmail.com>
2023-11-15 00:29:24 +0000

    Task 20

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# this is my sql 
+-- Write a script that prints thei full description of the 
+-- table first_table from thei database hbtn_0c_0 in your MySQL server.
+SELECT state, MAX(temperature)
+FROM temperatures
+GROUP BY state
+ORDER BY state;
