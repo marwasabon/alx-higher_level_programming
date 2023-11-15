5c74e671b8784cceb0b381c2795c8681c24459aa
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-15 02:14:30 +0200

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,5 +2,4 @@
 # this is my sql 
 -- Write a script that prints thei full description of the 
 -- table first_table from thei database hbtn_0c_0 in your MySQL server.
-SHOW FULL COLUMNS FROM first_table;
-
+DESCRIBE first_table
