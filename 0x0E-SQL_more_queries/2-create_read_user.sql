5d5148d34af9853ef4337e00ca5c8be6ec08fcc0
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-16 15:24:17 +0200

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,23 @@
+#!/usr/bin/env bash
 -- lists all privileges
 -- Checks if the database exists and create the database if not
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
--- Grants the user 
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
---- Grants Select to the user
+
+-- Check if the user exists and create the user if not
+DELIMITER $$
+CREATE PROCEDURE CreateUserIfNotExists(IN p_user VARCHAR(16), IN p_password VARCHAR(41))
+BEGIN
+  IF NOT EXISTS (SELECT 1 FROM mysql.user WHERE user = p_user) THEN
+    CREATE USER p_user@'localhost' IDENTIFIED BY p_password;
+  END IF;
+END$$
+DELIMITER ;
+
+-- Call the stored procedure to create the user
+CALL CreateUserIfNotExists('user_0d_2', 'user_0d_2_pwd');
+
+-- Grant SELECT privilege on the database to the user
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
--- FlUSH!!
+
+-- Make the changes take effect
 FLUSH PRIVILEGES;
