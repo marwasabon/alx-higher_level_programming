b5039d9a90b7050d7c93d1f6d9767b2b60b8144c
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-16 17:44:34 +0200

    Update 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 # this is my sql 
 -- lists all privileges
-SELECT * FROM cities 
-WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
+SELECT cities.id, cities.name
+FROM cities
+WHERE state_id = (
+ SELECT id
+ FROM states
+ WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
