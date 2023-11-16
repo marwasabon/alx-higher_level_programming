8b4554a97e7dbde635ca71603c919b9354b6c906
marwasabon <marwasabon@gmail.com>
2023-11-16 13:05:30 +0000

    task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # this is my sql
 -- Creating  the user script
-CREATE USER 'read_user'@'localhost' IDENTIFIED BY 'password';
+CREATE USER 'read_user'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Granting  SELECT privilege
 GRANT SELECT ON database_name.* TO 'read_user'@'localhost';
