4d6a1716ac1dbca40ff4d53d9ac73166f9fbb289
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-16 17:07:45 +0200

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,9 +2,8 @@
 # this is my sql 
 -- lists all privileges
 -- List all genres and the number of shows linked to each
-SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
-FROM tv_genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.name
-HAVING COUNT(tv_show_genres.show_id) > 0
-ORDER BY COUNT(tv_show_genres.show_id) DESC;
+SELECT tv_genres.name AS genre, COUNT(tv_genres.name) AS number_of_shows
+FROM tv_genres INNER JOIN tv_show_genres ON
+tv_genres.id = tv_show_genres.genre_id
+GROUP BY genre
+ORDER BY number_of_shows DESC
