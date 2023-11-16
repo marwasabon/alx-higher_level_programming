f9854680876cde5492ccecc1cae0e087dc86d22c
marwasabon <marwasabon@gmail.com>
2023-11-16 14:59:43 +0000

    task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,8 +4,8 @@
 SET @dexter_id = (SELECT id FROM tv_shows WHERE title = 'Dexter');
 
 -- List all genres of the show Dexter
-SELECT genres.name
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
+SELECT tv_genres.name
+FROM tv_genres
+INNER JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
 WHERE tv_show_genres.show_id = @dexter_id
-ORDER BY genres.name ASC;
+ORDER BY tv_genres.name ASC;
