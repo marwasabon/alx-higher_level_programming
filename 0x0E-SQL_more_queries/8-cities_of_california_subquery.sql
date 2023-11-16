a04de355045af648841361fc79efae848fd339f1
marwasabon <marwasabon@gmail.com>
2023-11-16 14:40:51 +0000

    task 8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # this is my sql 
 -- lists all privileges
-SET @california_id = (SELECT id FROM states WHERE name = 'California');
-
+SELECT * FROM cities 
+WHERE state_id = (SELECT id FROM states WHERE name = 'California')
 -- List all the cities of California from the cities table
 SELECT * FROM cities WHERE state_id = @california_id ORDER BY id ASC;
