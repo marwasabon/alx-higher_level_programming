f8b5f3342b9c97870aaa6d7aa5caad9a16b123df
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-16 17:24:01 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # this is my sql
 -- lists all privileges
-SELECT DISTINCT tv_genres.name FROM tv_genres
+SELECT DISTINCT tv_genres.name 
+FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
 INNER JOIN tv_shows
 ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title <> 'Dexter'
+AND tv_genres.name IS NOT NULL
 ORDER BY tv_genres.name ASC;
