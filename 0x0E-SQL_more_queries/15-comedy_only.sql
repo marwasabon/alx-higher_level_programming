3b735c5271f467330594be863bfcca1c4488d5c7
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-16 17:04:20 +0200

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -4,8 +4,9 @@
 SET @comedy_id = (SELECT id FROM tv_genres WHERE name = 'Comedy');
 
 -- List all Comedy shows
-SELECT tv_shows.title
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_show_genres.genre_id = @comedy_id
+SELECT tv_shows.title FROM tv_genres INNER JOIN tv_show_genres
+on tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON
+tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy'
 ORDER BY tv_shows.title ASC;
