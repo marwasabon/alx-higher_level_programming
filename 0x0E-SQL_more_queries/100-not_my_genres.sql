09bab60ab1845b66af2d1e3ea764af278920572c
Marwa Jaberallah Sabon <marwasabon@gmail.com>
2023-11-16 17:34:30 +0200

    Update 100-not_my_genres.sql

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 # this is my sql
 -- lists all privileges
-SELECT DISTINCT tv_genres.name 
+SELECT name
 FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id = tv_show_genres.genre_id
-INNER JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
-WHERE tv_shows.title <> 'Dexter'
-AND tv_genres.name IS NOT NULL
-ORDER BY tv_genres.name ASC;
+WHERE name NOT IN
+ (SELECT name
+  FROM tv_genres
+  LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+  LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+  WHERE tv_shows.title = 'Dexter')
+GROUP BY name
+ORDER BY name ASC
