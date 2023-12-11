f543ed55ef3a4966be278fbb71445938b4f7457b
marwasabon <marwasabon@gmail.com>
2023-12-11 10:41:41 +0000

    Task 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
+if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log(process.argv[2]);
 }
