f8bec7914493ab1c8fa87742c51f4013cff48520
marwasabon <marwasabon@gmail.com>
2023-12-14 20:19:10 +0000

    Task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
     cursor = db.cursor()
     cursor.execute(
             """
-            SELECT cities.id, cities.name, states.name
-            FROM cities INNER JOIN states ON states.id = cities.id
+            SELECT cities.id, cities.name, states.name FROM
+            cities INNER JOIN states ON states.id = cities.state_id
             """
             )
     rows = cursor.fetchall()
