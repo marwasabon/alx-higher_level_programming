aa891600536702f2859cced0b560fb91dd12fa7e
marwasabon <marwasabon@gmail.com>
2024-01-04 17:34:41 +0000

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" python script
+    demonstrate urlib package
+"""
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+        res = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(res)))
+        print("\t- content: {}".format(res))
+        print("\t- utf8 content: {}".format(res.decode('utf-8')))
