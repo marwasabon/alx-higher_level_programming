50c178819ead67fab9528f81983d1e1ae19e857c
marwasabon <marwasabon@gmail.com>
2024-01-05 11:43:18 +0000

    task_100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" python script
+    demonstrate urlib package
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+    from requests.auth import HTTPBasicAuth
+
+    repository = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            repository, owner)
+    res = requests.get(url)
+    commits = res.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
