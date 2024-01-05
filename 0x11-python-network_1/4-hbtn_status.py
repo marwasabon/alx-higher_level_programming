7d6fe0674019cbc63258ecc62091027fbc390c52
marwasabon <marwasabon@gmail.com>
2024-01-05 13:27:34 +0000

    task_4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -7,7 +7,7 @@
 if __name__ == '__main__':
     import requests
 
-    request = requests.get("https://intranet.hbtn.io/status")
+    response = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(request.text)))
-    print("\t- content: {}".format(request.text))
+    print(f"\t- type: {response.text}")
+    print(f"\t- content: {response.text}")
